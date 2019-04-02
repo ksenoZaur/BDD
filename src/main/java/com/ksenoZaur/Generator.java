ac34fb2e0d2189cd3dbb4738e047d47655beed15
davemustaine <ksenofontov97@mail.ru>
2019-04-03 01:53:22 +0700

    Заполняем методы 1, 2

---
+++ src/main/java/com/ksenoZaur/Generator.java
@@ -0,0 +1,8 @@
+package com.ksenoZaur;
+
+import java.util.ArrayList;
+
+public class Generator {
+    public void getPatterns() {
+    }
+}
--- src/main/java/com/ksenoZaur/MyStepdefs.java
+++ src/main/java/com/ksenoZaur/MyStepdefs.java
@@ -4,17 +4,29 @@ import cucumber.api.java.ru.Допустим;
 import cucumber.api.java.ru.Когда;
 import cucumber.api.java.ru.То;
 
+import java.util.ArrayList;
+
 public class MyStepdefs {
+
+    Generator generator;
+
     @Допустим("^Я имею класс генератор$")
     public void яИмеюКлассГенератор() {
+
+        this.generator = new Generator();
+
     }
 
     @Когда("^Я выбираю паттерн$")
     public void яВыбираюПаттерн() {
+
+        this.generator.getPatterns();
+
     }
 
     @То("^Я просматриваю список с доступными паттернами$")
     public void яПросматриваюСписокСДоступнымиПаттернами() {
+
     }
 
     @Когда("^Я выбираю язык$")
