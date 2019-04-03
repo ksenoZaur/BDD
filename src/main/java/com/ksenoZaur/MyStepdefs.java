0a8f9134224d4d4e883fb230a049628e2706a901
davemustaine <ksenofontov97@mail.ru>
2019-04-03 17:25:12 +0700

    Добавлен новый сценарий

--- src/main/java/com/ksenoZaur/MyStepdefs.java
+++ src/main/java/com/ksenoZaur/MyStepdefs.java
@@ -7,6 +7,10 @@ import cucumber.api.java.ru.Когда;
 import cucumber.api.java.ru.То;
 import org.junit.Assert;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.List;
 
 public class MyStepdefs {
@@ -15,8 +19,27 @@ public class MyStepdefs {
     List<String> patterns;
     List<String> languages;
 
+    String code;
     String currentPattern;
     String currentLanguage;
+
+    // Чтение кода из файла для проверки
+    private String readFromFile( String path ){
+        String text = null;
+
+        try {
+
+            text = new String(Files.readAllBytes(new File( path ).toPath()), StandardCharsets.UTF_8);
+            text = text.replaceAll("\\n", "");
+            text = text.replaceAll("\\r", "");
+            text = text.replaceAll("\\t", "");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text;
+    }
 
     @Допустим("^Я имею класс генератор$")
     public void яИмеюКлассГенератор() {
@@ -102,22 +125,26 @@ public class MyStepdefs {
     @И("^Я выбираю язык \"([^\"]*)\"$")
     public void яВыбираюЯзык(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
+        if( this.generator.getLanguages().indexOf(arg0) > -1 )
+            this.currentLanguage = arg0;
 
-        this.currentLanguage = this.generator.getLanguages().get( 0 );
         Assert.assertEquals( arg0, this.currentLanguage);
 
-        throw new PendingException();
+//        throw new PendingException();
     }
 
     @И("^Я нажимаю кнопку Generate$")
     public void яНажимаюКнопкуGenerate() {
 
-        this.generator.generate();
+        this.code = this.generator.generate();
 
     }
 
     @То("^Я получаю код, аналогичный коду в файле \"([^\"]*)\"$")
     public void яПолучаюКодАналогичныйКодуВФайле(String arg0) throws Throwable {
+
+        String expected = this.readFromFile("src/main/resources/com.ksenoZaur/input/" + arg0);
+        Assert.assertEquals(expected, this.code);
         // Write code here that turns the phrase above into concrete actions
 //        throw new PendingException();
     }
